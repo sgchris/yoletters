32370812369b58af8c00dcbe22c25cc91d54d04c
Greg Chris <sgchris@outlook.com>
2022-10-09 19:03:08 +0300

    fixed letter to image

--- src/components/LetterEdit.js
+++ src/components/LetterEdit.js
@@ -24,37 +24,34 @@ export default class LetterEdit extends React.Component {
 
     canvasTools = {
         save: () => {
-            debugger;
-            // margin size from the left and the right points of the letter
-            const marginSize = 10;
-
-            // get the leftest and the rightest points of the letter
+            // get the margins of the letter inside the drawing leftest and the rightest points of the letter
             let margins = this.canvasTools._findMargins();
+            let letterWidth = margins['rightest'] - margins['leftest'];
+            let letterHeight = margins['highest'] - margins['lowest'];
 
-            let drawingDims = this.canvasRef.current.canvas.drawing.getBoundingClientRect();
-            const drawingWidth = drawingDims.width;
-            const drawingHeight = drawingDims.height;
+            // get drawing canvas dims
             let gridDims = this.canvasRef.current.canvas.grid.getBoundingClientRect();
-            const gridWidth = gridDims.width;
             const gridHeight = gridDims.height;
 
             // create new canvas with new dims
             let newCanvas = document.createElement('canvas')
-            newCanvas.width = gridWidth;
-            newCanvas.height = gridHeight;
+            newCanvas.width = letterWidth; // width as the letter
+            newCanvas.height = gridHeight; // full height
+            newCanvas.style = "border: 1px solid red;";
             let newCanvasCtx = newCanvas.getContext('2d');
 
-            // draw the image into the context
-            let letterWidth = margins['rightest'] - margins['leftest'];
-            let newCanvasWidth = letterWidth + (2 * marginSize);
-            console.log('this.canvasRef.current', this.canvasRef.current.canvas.drawing);
-            newCanvasCtx.drawImage(this.canvasRef.current.canvas.drawing, 
-                marginSize, marginSize,
-                drawingWidth, drawingHeight);
+            // copy the letter into the new canvas
+            newCanvasCtx.drawImage(this.canvasRef.current.canvas.drawing,
+                margins['leftest'], 0,
+                letterWidth, gridHeight,
+                0, 0,
+                letterWidth, gridHeight);
 
+            // create an image out of it
             let img = new Image();
-            img.setAttribute('width', newCanvasWidth);
-            img.setAttribute('height', 400);
+            img.style="border: 1px solid #333";
+            img.setAttribute('width', letterWidth);
+            img.setAttribute('height', gridHeight);
             img.src = newCanvas.toDataURL("image/png");
             document.body.appendChild(img);
         },
@@ -68,22 +65,30 @@ export default class LetterEdit extends React.Component {
             if (!lines || lines.length === 0) {
                 return {leftest: -1, rightest: -1};
             }
- 
-            let leftest = 1.0 * this.canvasRef.current.props.canvasWidth;
+
+            let canvasProps = this.canvasRef.current.props
+            let boundery = canvasProps.brushRadius;
+            let leftest = 1.0 * canvasProps.canvasWidth;
             let rightest = 0;
+            let highest = 0;
+            let lowest = 1.0 * canvasProps.canvasHeight;
             lines.forEach(l => {
                 l.points.forEach(p => {
-                    if (p.x < leftest) {
-                        leftest = p.x;
-                    }
-                    if (p.x > rightest) {
-                        rightest = p.x;
-                    }
+                    if (p.x < leftest) leftest = p.x;
+                    if (p.x > rightest) rightest = p.x;
+
+                    if (p.y > lowest) lowest = p.y;
+                    if (p.y < highest) highest = p.y;
                 });
             });
 
             console.log('leftest', leftest, 'rightest', rightest);
-            return {leftest, rightest};
+            return {
+                leftest: leftest - boundery, 
+                rightest: rightest + boundery,
+                lowest: lowest - boundery,
+                highest: highest + boundery
+            };
         }
     }
 
